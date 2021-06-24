ffbfbcdf5cea571e33e1591abcf6ef14947cc1a4
s.rozdzynski <s.rozdzynski@xcitstudio.com>
2021-06-24 22:59:24 +0200

    server

--- config/default.cjs
+++ config/default.cjs
@@ -1,9 +1,14 @@
 const { nodeEnv } = require("./utils.cjs");
-const databaseConfig = require("../database/dbConfig.cjs");
+const { databaseConfig } = require("../database/dbConfig.cjs");
 
 module.exports = {
     env: nodeEnv,
     is_test: false,
     name: "restaurant-manager-js",
     database: databaseConfig,
+    options: {
+        port: process.env.PORT,
+        public_routes_prefix: "/api/public",
+    },
 };
+
